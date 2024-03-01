2b5faea9f00d629529d71c397dcf5816d4f10d41
Heli Joshi <helijoshi45@gmail.com>
2024-03-01 16:56:41 +0530

    updated index file

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
-let imgBox = document.querySelector('#img-box');
+let imgBox = document.querySelector('#imgBox');
 let qrImage = document.querySelector('#qrImage');
 let qrText = document.querySelector('#qrText');
 
 function generateQR(){
     qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
 }
